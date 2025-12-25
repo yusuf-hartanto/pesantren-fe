57a69380b51ec53dc6c619d8a7b980c0d2e774fe
Samsul Anwar <sam.aan46@gmail.com>
2025-12-25 10:36:07 +0700

    fixed useCannot

--- src/hooks/useCannot.ts
+++ src/hooks/useCannot.ts
@@ -3,9 +3,6 @@
 import type { Ability, Resource } from '@/types/permission'
 import { useCan } from './useCan'
 
-export function useCannot(
-  resource: Resource,
-  abilities: Ability | Ability[]
-): boolean {
-  return !useCan(resource, abilities)
+export function useCannot(resource: Resource, abilities: Ability | Ability[]): boolean {
+  return !useCan(abilities)
 }
