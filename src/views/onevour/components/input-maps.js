2b40410feebbdcb2a76c5edfd802e5a4a78d922c
Yusuf Hartanto <yusuf.hartanto69@gmail.com>
2026-01-17 21:44:31 +0700

    update:.nvmrc 20.19.3

--- src/views/onevour/components/input-maps.js
+++ src/views/onevour/components/input-maps.js
@@ -1,81 +1,81 @@
-import React, { useState, useEffect } from 'react'
+'use client'
 
-import MapPicker from 'react-google-map-picker'
+import React, { useState, useEffect, useCallback } from 'react'
+
+import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api'
 import { Box, debounce, TextField } from '@mui/material'
 
 const keyMap = process.env.NEXT_PUBLIC_KEY_MAP
 
-const MapGoogle = props => {
-  // ** Props
-  const { data, name, selected, gridProps, handleChange, color = 'grey' } = props
+const DEFAULT_LOCATION = {
+  lat: -6.9174639,
+  lng: 107.6191228
+}
 
-  const [defaultLocation, setDefaultLocation] = useState({ lat: Number('-6.9174639'), lng: Number('107.6191228') })
+const MapGoogle = ({ selected, gridProps, handleChange }) => {
+  const [location, setLocation] = useState(DEFAULT_LOCATION)
+  const [zoom, setZoom] = useState(12)
 
-  const [location, setLocation] = useState(defaultLocation)
-  const [zoom, setZoom] = useState(Number(12))
-  const [mounted, setMounted] = useState(false)
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: keyMap
+  })
 
-  function handleChangeLocation(lat, lng) {
-    setLocation({ lat: lat, lng: lng })
-    handleChange({ lat: lat, lng: lng })
-  }
+  const onMarkerDragEnd = useCallback(e => {
+    if (!e?.latLng) return
 
-  function handleChangeZoom(newZoom) {
-    setZoom(newZoom)
-  }
+    const lat = e.latLng.lat()
+    const lng = e.latLng.lng()
 
-  const handleChangeAddress = async data => {
-    if (data.target.value == '') return
+    setLocation({ lat, lng })
+    handleChange({ lat, lng })
+  }, [handleChange])
 
-    let region = data.target.value.replace(' ', '+')
-    let url = `https://maps.google.com/maps/api/geocode/json?key=${keyMap}&address=${region}&sensor=false&region=id`
+  const handleChangeAddress = async e => {
+    if (!e.target.value) return
 
-    const response = await fetch(url)
-    const { results, status } = await response.json()
+    const region = e.target.value.replace(/ /g, '+')
+    const url = `https://maps.google.com/maps/api/geocode/json?key=${keyMap}&address=${region}&region=id`
 
-    console.log(results)
+    const res = await fetch(url)
+    const { results } = await res.json()
 
-    if (results.length === 0) {
-      return
-    }
+    if (!results?.length) return
 
-    let lat = results[0].geometry.location.lat
-    let lng = results[0].geometry.location.lng
+    const { lat, lng } = results[0].geometry.location
 
-    setLocation({ lat: lat, lng: lng })
-    setDefaultLocation({ lat: lat, lng: lng })
-    handleChange({ lat: lat, lng: lng })
+    setLocation({ lat, lng })
+    handleChange({ lat, lng })
   }
 
   useEffect(() => {
     if (selected) {
-      setDefaultLocation(selected)
+      setLocation(selected)
     }
-
-    setTimeout(() => {
-      setMounted(true)
-    }, 1000)
   }, [selected])
 
   return (
-    <Box item {...gridProps}>
+    <Box {...gridProps}>
       <TextField
         fullWidth
         size='small'
-        label={'Search Location'}
-        placeholder={'Ex: Bandung Jawa Barat'}
-        sx={{ marginBottom: 2 }}
+        label='Search Location'
+        placeholder='Ex: Bandung Jawa Barat'
+        sx={{ mb: 2 }}
         onChange={debounce(handleChangeAddress, 1000)}
-      ></TextField>
-      {mounted && (
-        <MapPicker
-          defaultLocation={defaultLocation}
+      />
+
+      {isLoaded && (
+        <GoogleMap
+          center={location}
           zoom={zoom}
-          style={{ height: 400 }}
-          onChangeLocation={handleChangeLocation}
-          onChangeZoom={handleChangeZoom}
-          apiKey={keyMap}
-        />
+          mapContainerStyle={{ width: '100%', height: 400 }}
+        >
+          <Marker
+            position={location}
+            draggable
+            onDragEnd={onMarkerDragEnd}
+          />
+        </GoogleMap>
       )}
     </Box>
   )
